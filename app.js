63a50727c20a153e35d7c15d6af0f12723980474
Guillermo Acosta <guillermo222@att.net>
2021-11-12 21:13:57 -0800

    npm i dotenv and connect Database

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const mysql = require('mysql2');
-const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config({path: './.env'});
+
 
 const port = 3000;
+const config = {
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    port: process.env.port
+}
+
+const app = express();
+
+app.use( async(req, res, next) => {
+    try {
+        global.connection = await mysql.createConnection(config);
+        
+
+        // global.connection.connect();
+
+        // connection.query(`
+        //     SHOW DATABASES;
+        // ` ,  (err, res) => {
+        //     if (err) { console.log('Connection failed!',err);
+        //         }
+        //     console.log('Connected to database!',res)
+        // });
+
+        console.log('Connected to database!');
+
+        // connection.end()
+
+        await next();
+    } catch (err) {
+        console.log('Database Connection failed!', err);
+        res.status(500).send('Sorry, this server is down!');
+    } 
+}); 
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
-})
+    res.send('Hello world!');
+});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
